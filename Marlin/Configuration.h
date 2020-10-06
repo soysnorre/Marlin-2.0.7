b80c07aaf48d4be861ab716e6f3bb07f9cfe2e71
soysnorre <soysmorre@gmail.com>
2020-10-06 18:02:25 +0200

    Correción error en PREHEAT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1512,10 +1512,10 @@
 #define PREHEAT_2_TEMP_BED     75
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
-#define PREHEAT_2_LABEL       "ABS"
-#define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    110
-#define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
+#define PREHEAT_3_LABEL       "ABS"
+#define PREHEAT_3_TEMP_HOTEND 240
+#define PREHEAT_3_TEMP_BED    110
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
